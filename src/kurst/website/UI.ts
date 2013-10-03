3616d1a767fe7a85ecf599d935ee642b3591dba4
Karim Beyrouti <karim@kurst.co.uk>
2013-10-03 10:48:27 +0100

    UI - Cleaned up code

--- src/kurst/website/UI.ts
+++ src/kurst/website/UI.ts
@@ -8,28 +8,23 @@ module KurstWebsite {
 
         //--------------------------------------------------------------------------
 
-        private $           : JQueryStatic;
-        private expanded    : boolean = false;
         private fullScreen  : boolean = false;
 
         //--------------------------------------------------------------------------
 
-        constructor( $ : JQueryStatic  ) {
+        constructor( ) {
 
-            this.$ = $;
             this.init();
 
         }
 
         //--------------------------------------------------------------------------
 
-        /*
+        /**
          */
         public init() {
 
             $('#contentArea').hide();
-            //$('#contentArea').css( 'top' , '580px' );
-
             $( '#logoContainer' ).css('height','0px');
 
             $( '#logoContainer' ).delay( 1000 ).animate(  {
@@ -41,38 +36,14 @@ module KurstWebsite {
 
             this.showStaticContent( 1000 );
             this.initLinks();
-
             this.initFullScreenButton();
 
-
-        }
-        /*
-         */
-        public expandContent( flag : Boolean ) : void {
-
-            /*
-            if ( flag ) {
-
-                this.expanded = true;
-
-                $( '#contentArea' ).animate({
-                    top: '800px'
-                } , 500 , 'easeOutQuad' );
-
-            } else {
-
-                this.expanded = false;
-
-                $( '#contentArea' ).animate({
-                    top: '580px'
-                } , 1000 , 'easeOutQuad' );
-
-            }
-            */
         }
 
         //--------------------------------------------------------------------------
 
+	    /**
+	     */
         private launchFullScreen(element : Element ) {
 
             if(element['requestFullScreen']) {
@@ -83,8 +54,10 @@ module KurstWebsite {
                 element['webkitRequestFullScreen']();
             }
         }
-
+	    /**
+	     */
         private cancelFullscreen() {
+
             if(document['cancelFullScreen']) {
                 document['cancelFullScreen']();
             } else if(document['mozCancelFullScreen']) {
@@ -92,8 +65,10 @@ module KurstWebsite {
             } else if(document['webkitCancelFullScreen']) {
                 document['webkitCancelFullScreen']();
             }
-        }
 
+        }
+	    /**
+	     */
         private initFullScreenButton(){
 
             var controller : UI = this;
@@ -103,63 +78,52 @@ module KurstWebsite {
 
                 if ( controller.fullScreen )
                 {
-
+					// Exit full screen mode only for mobiles
                     if ( kurst.utils.JSUtils.isMobile() )
                     {
                         controller.cancelFullscreen();
                         $( '#logoContainer' ).fadeIn( 1000 );
-
-                        $( '#fullScreenButton').attr("src" , 'media/images/fullscreen_alt.png' );
-
+                        $( '#fullScreenButton').attr("src" , 'img/fullscreen_alt.png' );
                         $('#videoContainer').css( 'top' , '40px' );
                         $( '#videoContainer').width( '100%' );
                         $( '#videoContainer').height( '400px' );
                         $('#videoContainer').css( 'top' , '0px' );
-
                         $( '#contentArea' ).fadeIn( 1000 );
                         $("body").css("overflow", "visible");
-
                     }
                     else
                     {
-
-                        $( '#fullScreenButton').attr("src" , 'media/images/fullscreen_alt.png' );
-
+                        $( '#fullScreenButton').attr("src" , 'img/fullscreen_alt.png' );
                         $( '#videoContainer' ).width( '960px' );
                         $( '#videoContainer' ).height( '560px' );
                         $( '#videoContainer' ).css( 'top' , '40px' );
                         $( '#videoContainer' ).css( 'left' , '50%' );
                         $( '#videoContainer' ).css( 'margin-left' , '-480px' );
-
                         $( '#iFRameContent' ).width( '960px' );
                         $( '#iFRameContent' ).height( '560px' );
                         $( '#contentArea' ).fadeIn( 1000 );
                         $("body").css("overflow", "visible");
-
                     }
 
-
-
                     controller.fullScreen = false;
+
                 } else {
 
+	                // Only Launch full screen mode for mobiles
                     if ( kurst.utils.JSUtils.isMobile() )
                     {
                         controller.launchFullScreen(document.documentElement);
                         $( '#logoContainer' ).fadeOut( 250 );
                     }
 
-                    $( '#fullScreenButton').attr("src" , 'media/images/fullscreen_exit_alt.png' );
-
+                    $( '#fullScreenButton').attr("src" , 'img/fullscreen_exit_alt.png' );
                     $( '#videoContainer').width( '100%' );
                     $( '#videoContainer').height( '100%' );
                     $( '#videoContainer').css( 'top' , '0px' );
                     $( '#videoContainer').css( 'left' , '0px' );
                     $( '#videoContainer').css( 'margin-left' , '0px' );
-
                     $( '#iFRameContent').width( '100%' );
                     $( '#iFRameContent').height( '100%' );
-
                     $( '#contentArea' ).fadeOut( 250 );
                     $("body").css("overflow", "hidden");
 
@@ -173,8 +137,8 @@ module KurstWebsite {
 
 
         }
-        /*
-         */
+	    /**
+	     */
         private initLinks(){
 
             $( '#aboutUs-menuItem' ).click( function (){
@@ -195,8 +159,8 @@ module KurstWebsite {
             });
 
         }
-        /*
-         */
+	    /**
+	     */
         private showStaticContent( delayTime ) {
 
             $( '#contentArea' ).delay( delayTime ).fadeIn( 1000 );
